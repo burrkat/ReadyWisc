95f1c63f72db719bac7c26c5ff6c75c6bff8d716
David Krawchuk <krawchukdavid@gmail.com>
2015-02-14 21:13:39 -0600

    Added the sql contract for the checklist permanent store.

---
+++ app/src/main/java/edu/parkside/cs/checklist/Checklist_Contract.java
@@ -0,0 +1,38 @@
+package edu.parkside.cs.checklist;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by krawchukd on 2/14/15.
+ */
+public final class Checklist_Contract {
+
+    // Stubbed to prevent instantiation.
+    public Checklist_Contract() {};
+
+    /* Inner class that defines the table contents */
+    public static abstract class Checklist implements BaseColumns {
+        public static final String TABLE_NAME = "checklist";
+        public static final String COLUMN_NAME_ENTRY_ID = "entryid";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_COMPLETE = "complete";
+    }
+
+    /* Inner class that defines the table contents */
+    public static abstract class Item implements BaseColumns {
+        public static final String TABLE_NAME = "item";
+        public static final String COLUMN_NAME_ENTRY_ID = "entryid";
+        public static final String COLUMN_NAME_NAME = "name";
+        public static final String COLUMN_NAME_QTY = "qty";
+        public static final String COLUMN_NAME_COMPLETE = "complete";
+        public static final String COLUMN_NAME_CHECKLIST_ID = "checklist_entryid";
+    }
+
+    /* Inner class that defines the table contents */
+    public static abstract class Description implements BaseColumns{
+        public static final String TABLE_NAME = "description";
+        public static final String COLUMN_NAME_ENTRY_ID = "entryid";
+        public static final String COLUMN_NAME_DESCRIPTION = "description";
+        public static final String COLUMN_NAME_ITEM_ID = "item_entryid";
+    }
+}
